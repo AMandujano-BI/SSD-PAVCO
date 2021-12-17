4cc5e27565639ac0c54af30bc2368a46502b28ac
Fernando <fernandomercado529@gmail.com>
2021-12-17 10:53:06 -0400

    Add user controller

---
+++ resources/js/Pages/User/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <app-layout title="Companies">
+    <div class="p-5 mx-auto container">
+      <h1 class="text-center text-2xl p-5 font-bold">List Users</h1>
+    </div>
+  </app-layout>
+</template>
+
+<script>
+import AppLayout from "@/Layouts/AppLayout.vue";
+
+import ModalVue from "@/Jetstream/Modal.vue";
+export default {
+  components: {
+    AppLayout: AppLayout,
+    Modal: ModalVue,
+  },
+};
+</script>
+<style>
+</style>
