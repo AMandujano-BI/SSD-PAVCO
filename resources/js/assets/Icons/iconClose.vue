e6dfaad723442c3f4de0941917ef6718cf878a89
Fernando <fernandomercado529@gmail.com>
2021-12-20 17:32:55 -0400

    add svg icons

---
+++ resources/js/assets/Icons/iconClose.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg width="25" height="25" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg">
+    <g fill="none" fill-rule="evenodd">
+        <path fill="#17C4B2" d="M0 0h25v25H0z"/>
+        <path stroke="#FFF" stroke-linecap="round" d="m7 7 11.129 11.129M18.129 7 6.999 18.129"/>
+    </g>
+</svg>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/assets/Icons/iconEmail.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg width="25" height="25" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg">
+    <g fill="none" fill-rule="evenodd">
+        <path fill="#50BEFF" d="M0 0h25v25H0z"/>
+        <path d="M19.5 7h-13C5.673 7 5 7.673 5 8.5v9c0 .827.673 1.5 1.5 1.5h13c.827 0 1.5-.673 1.5-1.5v-9c0-.827-.673-1.5-1.5-1.5zm0 1c.068 0 .133.014.192.039L13 13.839l-6.692-5.8A.498.498 0 0 1 6.5 8h13zm0 10h-13a.5.5 0 0 1-.5-.5V9.095l6.672 5.783a.5.5 0 0 0 .656 0L20 9.095V17.5a.5.5 0 0 1-.5.5z" fill="#FFF" fill-rule="nonzero"/>
+    </g>
+</svg>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
