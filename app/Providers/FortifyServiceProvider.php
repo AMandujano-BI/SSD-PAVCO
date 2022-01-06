32cdb9f548d93cc55db3d2db76c1e5ad97ec7d5d
Fernando <fernandomercado529@gmail.com>
2022-01-06 14:18:27 -0400

    improve styles in login

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -37,7 +37,6 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
 
         RateLimiter::for('login', function (Request $request) {
-            // dd($request);
             return Limit::perMinute(5)->by($request->username.$request->ip());
         });
 
