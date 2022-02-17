145a14b61d9fcb69b53a84e8b50d8913fabf5ffc
Fernando <fernandomercado529@gmail.com>
2022-02-17 13:12:57 -0400

    Add fields for hours in table

---
+++ database/migrations/2022_02_17_171059_add_fields_parts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldsPartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('parts', function (Blueprint $table) {
+            $table->string('typePlateThick')->nullable();
+            $table->integer('hoursWs')->nullable();
+            $table->integer('hoursRs')->nullable();
+            $table->boolean('isWs')->nullable();
+            $table->boolean('isRs')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('parts', function (Blueprint $table) {
+            $table->dropColumn('typePlateThick', 'hoursWs', 'hoursRs', 'isWs', 'isRs');
+        });
+    }
+}
